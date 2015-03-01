b65bfbeb97ee31aa2b67bc3cddd222e6d40b9a42
A A <bertmaistro1@gmail.com>
2015-03-01 14:31:47 -0500

    styling, product page

---
+++ www/scripts/controllers/nav.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoNovisApp.controller:NavCtrl
+ * @description
+ * # NavCtrl
+ * Controller of the yoNovisApp
+ */
+angular.module('yoNovisApp')
+  .controller('NavCtrl', function ($scope, $location) {
+    $scope.test = "TEST";
+
+    $scope.isActive = function(route) {
+        return route === $location.path();
+    }
+  });
